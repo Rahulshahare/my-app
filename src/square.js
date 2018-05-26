12d6781a9e717c9f6f8351caa1804a1e8e4087a6
rahul shahare <rahulshahare@gmail.com>
2018-05-27 01:12:47 +0530

    Props will show value

--- src/square.js
+++ src/square.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Square extends React.Component{
     render(){
         return(
-            <button></button>
+            <button className="square">
+                {this.props.value}
+            </button>
         );
     }
 }
