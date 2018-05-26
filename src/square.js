5f5fd64f30ae3d345edda98caafb1f07c8102783
rahul shahare <rahulshahare@gmail.com>
2018-05-27 01:21:01 +0530

    Working with state of component

--- src/square.js
+++ src/square.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class Square extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value: null,
+        };
+    }
     render(){
         return(
             <button className="square" onClick={()=>alert('Click')}>
