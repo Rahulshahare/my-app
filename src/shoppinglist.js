46928bb7464b1f679f4676d0f572cb0242014172
rahul shahare <rahulshahare@gmail.com>
2018-05-27 00:45:16 +0530

    Re-write the shopping list component in diff file

---
+++ src/shoppinglist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ShoppingList extends React.Component{
+    render(){
+        return(
+            <div className="shopping-list">
+                <h1>Shopping list for</h1>
+                <ul>
+                    <li>instagram</li>
+                    <li>whatsaap</li>
+                    <li>tinder</li>
+                </ul>
+            </div>
+        );
+    }
+    
+}
+
+export default ShoppingList;
