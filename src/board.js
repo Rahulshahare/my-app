0342667b3558d8b014e3e0fcfde34a2645894301
rahul shahare <rahulshahare@gmail.com>
2018-05-27 01:42:51 +0530

    Constant is used

--- src/board.js
+++ src/board.js
@@ -7,9 +7,15 @@ class Board extends React.Component{
             <Square value={i}/>
         )
     }   
+
+
     render(){
+        const status = 'Next Player: X';
+
+
         return(
-            <div className="board">
+            <div>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
