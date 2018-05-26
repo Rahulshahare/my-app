e81daf2ac18006c84d87f0e0b1117eed5ecbe818
rahul shahare <rahulshahare@gmail.com>
2018-05-27 01:08:46 +0530

    board will render 9 square

--- src/board.js
+++ src/board.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Square from './square';
 
 class Board extends React.Component{
     render(){
         return(
+            <div className="board">
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
 
+            </div>
         );
     }
 }
