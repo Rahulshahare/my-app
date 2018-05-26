6fe05aa0d4776c5436c03823dba5684b178240c5
rahul shahare <rahulshahare@gmail.com>
2018-05-27 01:15:25 +0530

    it is Working ...Wow

--- src/board.js
+++ src/board.js
@@ -6,19 +6,19 @@ class Board extends React.Component{
         return(
             <div className="board">
                 <div className="1">
-                    <Square/>
-                    <Square/>
-                    <Square/>
+                    <Square value={1} />
+                    <Square value={2} />
+                    <Square value={3} />
                 </div>
                 <div className="2">
-                    <Square/>
-                    <Square/>
-                    <Square/>
+                    <Square value={4} />
+                    <Square value={5} />
+                    <Square value={6} />
                 </div>
                 <div className="3">
-                    <Square/>
-                    <Square/>
-                    <Square/>
+                    <Square value={7} />
+                    <Square value={8} />
+                    <Square value={9} />
                 </div>
 
             </div>
