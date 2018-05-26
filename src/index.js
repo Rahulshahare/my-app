3705f51c01546cdb16a8c7f137edb40d50a028c9
rahul shahare <rahulshahare@gmail.com>
2018-05-27 00:41:58 +0530

    this shoppingList Component is Working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class ShoppingList extends React.Component{
+    render(){
+        return(
+            <div className="shopping-list">
+                <h1>Shopping list for</h1>
+                <ul>
+                    <li>instagram</li>
+                    <li>whatsaap</li>
+                    <li>tinder</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ShoppingList/>,document.getElementById('root'));
