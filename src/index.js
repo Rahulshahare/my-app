6521b8a267769cbf12da85289352e530dacb1803
rahul shahare <rahulshahare@gmail.com>
2018-05-27 01:08:56 +0530

    game will render Board

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Board from './board';
 class Game extends React.Component{
     render(){
         return(
