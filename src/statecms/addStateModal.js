c23057586058deffa47a7d938f41795b507f7ddb
rahul shahare <rahulshahare@gmail.com>
2018-05-30 15:30:47 +0530

    Model For Adding A State

---
+++ src/statecms/addStateModal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '../button';
+
+class AddStateModel extends React.Component{
+    render(){
+        if(!this.state.ShowModel){
+            return false;
+        }
+        
+        return(
+
+        )
+    }
+}   
+
+export default AddStateModel;
