cc4b3d2055f1ae8b07db3d4788bb69a1f44ebe57
rahul shahare <rahulshahare@gmail.com>
2018-05-30 14:12:09 +0530

    its working

--- src/statecms/state.js
+++ src/statecms/state.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import StateMap  from './statemap';
+
+
 
 class State extends React.Component{
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          states: [],
+        };
+      }
+    
+      componentDidMount() {
+        fetch('http://localhost/oceangreen/admin/api/readState.php?Key=Authority')
+        .then(blob => blob.json())
+        .then(data => {
+          console.log(data);
+          this.setState({states: data})
+          //alert("this is New state"  + this.state.states);
+          
+        })
+        .catch(e => {
+          console.log(e);
+          return e;
+        });
+      }
+
+      editing(id){
+        alert("You are editing state with id " + id);
+      }
+      deleting(id){
+          alert("Are you sure to delete state of id "+ id);
+      }
+
+    
     render(){
         return(
-            <h1>Hello World</h1>
+            <StateMap 
+                StatesData={this.state.states}
+                editing = {this.editing}
+                deleting = {this.deleting}
+            />
         )
     }
 }
-
 
 export default State;
